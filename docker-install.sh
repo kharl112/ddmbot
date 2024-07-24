2dbac38ad524984b4f1128c2d260ab551aa541a6
root <kharl.dms112@gmail.com>
2024-07-24 12:44:35 +0000

    add comment for dev install

--- docker-install.sh
+++ docker-install.sh
@@ -5,6 +5,9 @@ if [ -n "$(which docker)" ]; then
 	docker rm ddmbot
 	docker image rm ddmbot
 	docker build -t ddmbot:latest .
+
+  #for development
+	#docker run -d --name ddmbot --env-file .env -v /apps/ddmbot/node_modules -v "$PWD":/apps/ddmbot -t ddmbot:latest
 	docker run -d --name ddmbot --env-file .env -t ddmbot:latest
 else
 	echo no docker found
