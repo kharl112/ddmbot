390f8011d26ce8c89569a5441b0ba0bfa357ec59
root <kharl.dms112@gmail.com>
2024-08-24 16:34:47 +0000

    fixed bot

--- command-builders/controllers/music.js
+++ command-builders/controllers/music.js
@@ -1,4 +1,13 @@
-const { createAudioPlayer, joinVoiceChannel, createAudioResource, VoiceConnectionStatus, entersState, NoSubscriberBehavior, getVoiceConnection, AudioPlayerStatus  } = require('@discordjs/voice');
+const { 
+  createAudioPlayer, 
+  joinVoiceChannel,
+  createAudioResource, 
+  VoiceConnectionStatus,
+  entersState, 
+  NoSubscriberBehavior, 
+  getVoiceConnection,
+  AudioPlayerStatus  
+} = require('@discordjs/voice');
 
 const { client } = require('../../main.js');
 
@@ -21,93 +30,148 @@ module.exports = (() => {
     return createAudioResource(stream);
   }
 
+  const get_song = async (song_query) => {
+    const songInfo = await yts(song_query);
+
+    if(!songInfo.all.length)  return null;
+
+    return new_song = {
+      title: songInfo.all[0].title,
+      url: songInfo.all[0].url,
+    }
+
+  }
+
+  const get_resource_from_queue = (channel_id)  => {
+    if(!map_songs[channel_id].length) return null;
+    let song = map_songs[channel_id].shift();
+    return { resource: get_audio_resource_stream(song), song } ;
+  }
+
   const play = async (interaction) => {
-    try {
-      const options = {
-        channelId: interaction.member.voice.channelId,
-        guildId: interaction.guildId,
-        adapterCreator: interaction.member.voice.guild.voiceAdapterCreator,
+
+    //user needs to wait (bot is thinking...)
+    await interaction.deferReply();
+
+    const member_channel_id = interaction.member.voice.channelId;
+    //check if user is on a voice channel 
+    if(!member_channel_id) {
+      await interaction.editReply({
+        content: `you're not in a voice channel`,
+        ephemeral: true
+      });
+
+      return;
+    }
+
+    //set options for the connection
+    const options = {
+      channelId: member_channel_id,
+      guildId: interaction.guildId,
+      adapterCreator: interaction.member.voice.guild.voiceAdapterCreator,
+    }
+
+    //get connection
+    let connection = getVoiceConnection(options.guildId);
+
+    //create new connection if theres no connection
+    if(!connection){
+      connection = joinVoiceChannel(options);
+    }
+
+    //check if bot is connected to another channel
+    else if(connection.packets.state && connection.packets.state.channel_id && member_channel_id != connection.packets.state.channel_id){
+      await interaction.editReply({
+        content: `bot is on another channel`,
+        ephemeral: true
+      });
+
+      return;
+    }
+
+    //create audio player
+    const player = createAudioPlayer({
+      behaviors: {
+        noSubscriber: NoSubscriberBehavior.Pause
+      },
+    });
+
+    //subscription
+    connection.subscribe(player);
+
+    //when bot intentionally/unintentionally disconnected
+    connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
+      try {
+        await Promise.race([
+          entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
+          entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
+        ]);
+      } catch (error) {
+
+        //garbage collection
+        delete map_songs[member_channel_id];
+        player.stop();
+
+        //clear music queue
+        connection.destroy();
+        await interaction.editReply({
+          content: `Bro got disconnected`, 
+        });
+
+        return;
       }
-
-      await interaction.deferReply();
-
-      const song_query = interaction.options._hoistedOptions[0].value;
-      const songInfo = await yts(song_query);
-
-      const new_song = {
-        title: songInfo.all[0].title,
-        url: songInfo.all[0].url,
-      };
+    }); 
 
 
-      let connection = getVoiceConnection(options.guildId);
-      if(!connection){
-        connection = joinVoiceChannel(options);
-      }
+    //get song from yt
+    const song_query = interaction.options._hoistedOptions[0].value;
+    const new_song = await get_song(song_query);
 
-      if(!map_songs.hasOwnProperty(options.channelId)){
-        map_songs[options.channelId] = [new_song]
-      }else{
-        map_songs[options.channelId] = [...map_songs[options.channelId], new_song];
+    //ignore if song does not exists
+    if(!new_song) {
+      await interaction.editReply({
+        content: `no audio found for query: ${song_query}`,
+      });
+
+      return;
+    }
+
+    //TODO: this should be on sqlite 
+    //add new song to queue
+    if(!map_songs.hasOwnProperty(member_channel_id)){
+      map_songs[member_channel_id] = [new_song]
+    } else{
+      //check again if theres no connection
+      if(getVoiceConnection(options.guildId)){
+        map_songs[member_channel_id] = [...map_songs[member_channel_id], new_song];
         await interaction.editReply({
           content: `added on queue ${new_song.title}`,
         });
-        return;
       }
-
-      let song = map_songs[options.channelId].shift();
-      let resource = get_audio_resource_stream(song);
-
-      const subscription = connection.subscribe(player); 
-
-      player.play(resource);
-      const channel = client.channels.cache.get(options.channelId);
-
-      player.on('error', async (error) => {
-        if(map_songs[options.channelId].length){
-          song = map_songs[options.channelId].shift();
-          let resource = get_audio_resource_stream(song);
-          player.play(resource);
-          await interaction.followUp(`playing rn ${song.title}`);
-        }else {
-          console.log(error);
-          await interaction.followUp('I left');
-        }
-      });
-
-      player.on(AudioPlayerStatus.Idle, async () => {
-        if(map_songs[options.channelId].length){
-          song = map_songs[options.channelId].shift();
-          let resource = get_audio_resource_stream(song);
-          player.play(resource);
-          await interaction.followUp(`playing rn ${song.title}`);
-        }else {
-          console.log('idled');
-          connection.destroy();
-          await interaction.followUp('I left');
-        }
-      });
-
-
-      connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
-        try {
-          await Promise.race([
-            entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
-            entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
-          ]);
-        } catch (error) {
-          connection.destroy();
-        }
-      });
-
-      await interaction.editReply({
-        content: `playing rn ${song.title}`,
-      });
-
-    } catch (error) {
-      console.log(error);
-      await interaction.reply({ content: error, ephemeral: true });
+      return;
     }
+
+    let current = get_resource_from_queue(member_channel_id);
+    if(current) {
+      player.play(current.resource);
+    }
+
+    player.on('error', (error) => {
+      console.error(`Error: ${error.message}`);
+    });
+
+    player.on(AudioPlayerStatus.Idle, () => {
+      current = get_resource_from_queue(member_channel_id);
+      if(current) {
+        player.play(current.resource);
+      }
+    });
+
+    player.on(AudioPlayerStatus.Playing, async () => {
+      console.log(map_songs);
+      if(current) await interaction.followUp(`playing rn ${current.song.title}`);
+    });
+
   };
 
   return { play };
